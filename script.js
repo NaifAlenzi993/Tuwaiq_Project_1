3a5e9f06236e8ae068ed7b92e652525ceb91911a
NaifAlenzi993 <Naif.Alryadh.rae.js@tuwaiq.edu.sa>
2021-10-20 15:04:56 +0300

    day4

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let Books = [
     author: "Dave Grohl",
     description: "The Storyteller: Tales of Life and Music",
     color: "black",
+    price: "19 $",
     idf: rndStr(6),
     idx: rndStr(6),
   },
@@ -24,6 +25,7 @@ let Books = [
     description:
       "To Rescue the Republic: Ulysses S. Grant, the Fragile Union, and the Crisis of 1876",
     color: "black",
+    price: "60 $",
     idf: rndStr(6),
     idx: rndStr(6),
   },
@@ -33,6 +35,7 @@ let Books = [
     description:
       "Midnight in Washington: How We Almost Lost Our Democracy and Still Could",
     color: "black",
+    price: "51 $",
     idf: rndStr(6),
     idx: rndStr(6),
   },
@@ -42,6 +45,7 @@ let Books = [
     description:
       "The Dying Citizen: How Progressive Elites, Tribalism, and Globalization Are Destroying the Idea of America",
     color: "black",
+    price: "35 $",
     idf: rndStr(6),
     idx: rndStr(6),
   },
@@ -50,6 +54,7 @@ let Books = [
     author: "Liane Moriarty",
     description: "Apples Never Fall",
     color: "black",
+    price: "20 $",
     idf: rndStr(6),
     idx: rndStr(6),
   },
@@ -58,6 +63,7 @@ let Books = [
     author: "Frank Herbert",
     description: "Dune (Dune Chronicles, Book 1)",
     color: "black",
+    price: "55 $",
     idf: rndStr(6),
     idx: rndStr(6),
   },
@@ -65,6 +71,7 @@ let Books = [
     url: "https://images-na.ssl-images-amazon.com/images/I/81Ok1VQoXWS._AC_UL200_SR200,200_.jpg",
     author: "Vanderbilt: The Rise and Fall of an American…",
     description: "Anderson Cooper",
+    price: "43 $",
     idf: rndStr(6),
     idx: rndStr(6),
   },
@@ -79,14 +86,12 @@ let navbarSelect = document.querySelectorAll("#navVar1 a");
 
 let transTime = 150;
 
-let pages = [
-  $("#bookslist"),
-  $("#Favoritelist"),
-  $(".signup-form"),
-  $(".login-form"),
-];
 
-function renderArr(array, DivSelect, ShowX = false) {
+let users = [{username: "admin" , mail: "admin@admin.cc" , pass: "admin" , admin: true , fav : bookLike}]
+let userOnline = []
+
+
+function mkHtmlFromArr(array, DivSelect, ShowX = false) {
   let codeX = "";
 
   for (let i = 0; i < array.length; i++) {
@@ -99,7 +104,7 @@ function renderArr(array, DivSelect, ShowX = false) {
           <h4>${array[i].author}</h4>
           <p href="#">${array[i].description}</p>
       </span> 
-      <p href="#" style = "font-size: 20px"><b>23$</b></p>  
+      <p href="#" style = "font-size: 20px"><b>${array[i].price}</b></p>  
       <i id = "icon-${array[i].idf}" class="fas fa-star icon-f" style = "color: ${array[i].color}"></i>
       
       ${codeX}
@@ -109,7 +114,7 @@ function renderArr(array, DivSelect, ShowX = false) {
   }
 }
 
-renderArr(Books, mainItems, true);
+mkHtmlFromArr(Books, mainItems, true);
 
 function Clear() {
   document.querySelector(".login-form #username-login").value = "";
@@ -118,14 +123,31 @@ function Clear() {
   document.querySelector(".signup-form #username-signup").value = "";
   document.querySelector(".signup-form #mail-signup").value = "";
   document.querySelector(".signup-form #pass-signup").value = "";
+
+  document.querySelector(".form-addbook #url-image").value = "";
+  document.querySelector(".form-addbook #author").value = "";
+  document.querySelector(".form-addbook #description").value = "";
+  document.querySelector(".form-addbook #price").value = "";
+  
 }
 
 //event function btn
+$("#Addbook").click(function () {
+  $(".form-addbook").css("display", "flex");
+  $("#bookslist").hide(transTime);
+  $(".signup-form").hide(transTime);
+  $(".login-form").hide(transTime);
+  $("#Favoritelist").hide(transTime);
+
+  Clear();
+});
+
 $("#SignIn").click(function () {
   $("#bookslist").hide(transTime);
   $(".signup-form").hide(transTime);
   $(".login-form").css("display", "flex");
   $("#Favoritelist").hide(transTime);
+  $(".form-addbook").hide(transTime);
 
   Clear();
 });
@@ -135,7 +157,7 @@ $("#home").click(function (e) {
   $(".login-form").hide(transTime);
   $(".signup-form").hide(transTime);
   $("#Favoritelist").hide(transTime);
-
+  $(".form-addbook").hide(transTime);
   Clear();
 });
 
@@ -144,18 +166,24 @@ $("#SignUp").click(function () {
   $(".login-form").hide(transTime);
   $(".signup-form").css("display", "flex");
   $("#Favoritelist").hide(transTime);
+  $(".form-addbook").hide(transTime);
 
   Clear();
 });
+
 
 $("#Favorite").click(function () {
   $("#bookslist").hide(transTime);
   $(".signup-form").hide(transTime);
   $(".login-form").hide(transTime);
+  $(".form-addbook").hide(transTime);
   $("#Favoritelist").css("display", "flex");
 
   Clear();
 });
+
+
+
 
 // Add like Book + change color icon
 let selectElementHome = document.querySelectorAll("#items");
@@ -167,7 +195,7 @@ selectElementHome.forEach((e) => {
       if ("icon-" + Books[i].idx == e.target.id) {
         Books.splice(i, 1);
         mainItems.html("");
-        renderArr(Books, mainItems, true);
+        mkHtmlFromArr(Books, mainItems, true);
         break;
       }
 
@@ -177,13 +205,13 @@ selectElementHome.forEach((e) => {
           mainItemsF.html("");
           Books[i].color = "gold";
           bookLike.push(Books[i]);
-          renderArr(bookLike, mainItemsF);
+          mkHtmlFromArr(bookLike, mainItemsF);
         } else {
           bookLike.forEach((element, index) => {
             if (Books[i].url == element.url) {
               bookLike.splice(index, 1);
               mainItemsF.html("");
-              renderArr(bookLike, mainItemsF);
+              mkHtmlFromArr(bookLike, mainItemsF);
               e.target.style.color = "black";
               Books[i].color = "black";
             }
@@ -194,96 +222,118 @@ selectElementHome.forEach((e) => {
   });
 });
 
-// starSelect.forEach((element , i) => {
-//   element.addEventListener("click", (e) => {
-//     if (e.target.style.color == "black") {
 
-//       //after Like Clear Favoritelist Convert Color To Gold And Add Obj to BookLike[] And Make List
-//           e.target.style.color = "gold";
-//           mainItemsF.html("");
-//           Books[i].color = "gold"
-//           bookLike.push(Books[i]);
-//           renderArr(bookLike , mainItemsF);
+let selectElementF = document.querySelectorAll("#items-f");
 
-//     } else {
+selectElementF.forEach((element , i) => {
+  element.addEventListener("click", (e) => {
+    if (e.target.style.color == "gold") {
+      for(o in bookLike){
+        if ("icon-"+bookLike[o].idf == e.target.id) {
+            let indexOf = Books.indexOf(bookLike[o])
+            Books[indexOf].color = "black";
+            bookLike.splice(o , 1);
+            mainItemsF.html("");
+            mkHtmlFromArr(bookLike, mainItemsF);
 
-//       //unLike And Convert To Black and Delete Item from Favoritelist
-//       bookLike.forEach((element,index) => {
-//         if (Books[i].url == element.url) {
-//           bookLike.splice(index,1);
-//           mainItemsF.html("");
-//           renderArr(bookLike , mainItemsF);
-//           e.target.style.color = "black";
-//           Books[i].color = "black"
-//         }
-//       });
+            mainItems.html("");
+            mkHtmlFromArr(Books, mainItems, true);
 
-//     }
-//   });
-// });
+        }
+      }
 
-// //Delete Book From Home
-// let xSelect = document.querySelectorAll("#icon-x");
-// xSelect.forEach((element , i) => {
-//   element.addEventListener("click", (e) => {
-
-//       console.log(Books[i]);
-//       Books.splice(i,1);
-//       mainItems.html("");
-//       renderArr(Books, mainItems , true);
-
-//   })
-// });
-
-// let starSelectF = document.querySelectorAll("#items-f");
-// let home = document.getElementById("#items");
-// starSelectF.forEach((element , i) => {
-//   element.addEventListener("click", (e) => {
-//     switch (e.target.id) {
-//       case "icon-f":
-//         if (e.target.style.color == "gold") {
-//           for (o in Books) {
-//             if (bookLike[i].url == Books[o].url) {
-//               bookLike.splice(i, 1);
-//               Books[o].color = "black";
-//               //clear fav list
-//               mainItemsF.html("");
-//               renderArr(bookLike, mainItemsF);
-
-//               //clear home
-//               mainItems.html("");
-//               renderArr(Books, mainItems , true);
-//               break;
-//             }
-//           }
-//         }
-
-//         break;
-//     }
-//   });
-// });
-
-// starSelectF.forEach((element , i) => {
-//   element.addEventListener("click", (e) => {
-//     console.log(starSelectF);
-//     if (e.target.style.color == "gold") {
-
-//       console.log("GDFHGHD");
-
-//     }
-//   });
-// });
-
-$("#btn-login").click(() => {
-  let user = document.querySelector(".login-form #username");
-  let pass = document.querySelector(".login-form #pass");
-
-  console.log(user.value + " " + pass.value);
+    }
+  });
 });
 
-//Sign Up
-$("#btn-Rigstry").click(() => {
-  let user = document.querySelector(".signup-form #username");
-  let mail = document.querySelector(".signup-form #mail");
-  let pass = document.querySelector(".signup-form #pass");
-});
+
+function addBook(){
+  let urlImage = document.querySelector(".form-addbook #url-image").value ;
+  let authorImage = document.querySelector(".form-addbook #author").value ;
+  let descriptionImg = document.querySelector(".form-addbook #description").value ;
+  let priceBook = document.querySelector(".form-addbook #price").value ;
+  
+  let newBook = {url: urlImage ,
+    author: authorImage , 
+    description: descriptionImg,
+    price: priceBook,
+    color: "black",
+    idx: rndStr(6),
+    idf: rndStr(6)};
+   
+    Books.push(newBook);
+
+    mainItems.html("");
+
+    mkHtmlFromArr(Books, mainItems, true);
+
+    Clear();
+
+}
+
+
+function loginAcc(){
+  let usernameVa = document.querySelector(".login-form #username-login").value;
+  let passVa = document.querySelector(".login-form #pass-login").value;
+  let result = $("#result")
+ 
+  let localsrotage = localStorage.static;
+  if (localsrotage == null){
+    result.html(`<p id="result"> error 10 </p>`)
+    result.css("color" , "red")
+    return null
+  }
+
+  let staticLocal = JSON.parse(localsrotage);
+  
+  
+  for(i in staticLocal){
+    if (staticLocal[i].username == usernameVa && staticLocal[i].pass == passVa){
+      
+      result.html(`<p id="result"> login in </p>`)
+      result.css("color" , "green")
+      break;
+    }else{
+      result.html(`<p id = "result" > *error </p>`)
+       result.css("color" , "red")
+      break;
+    }
+  }
+  
+
+}
+
+function addNewAccount(){
+  let usernameVar = document.querySelector(".signup-form #username-signup").value;
+  let mailVar = document.querySelector(".signup-form #mail-signup").value;
+  let passVar = document.querySelector(".signup-form #pass-signup").value;
+  
+  let newAccount = {username: usernameVar , mail: mailVar ,  pass: passVar , admin: false , fav: bookLike}
+  users.push(newAccount);
+
+  localStorage["static"] = JSON.stringify(users);
+  
+
+}
+
+
+
+
+
+
+// $("#btn-login").click(() => {
+//   let user = document.querySelector(".login-form #username");
+//   let pass = document.querySelector(".login-form #pass");
+
+//   console.log(user.value + " " + pass.value);
+// });
+
+// //Sign Up
+// $("#btn-Rigstry").click(() => {
+//   let user = document.querySelector(".signup-form #username");
+//   let mail = document.querySelector(".signup-form #mail");
+//   let pass = document.querySelector(".signup-form #pass");
+// });
+
+
+
